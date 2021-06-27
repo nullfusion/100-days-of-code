a111c00ab9ce026aaa67c96d9066b049d0330806
NullFusion <85862537+nullfusion@users.noreply.github.com>
2021-06-27 18:57:37 +0530

    fully implemented linkedlist and added null checks

--- src/data-structures/linkedlist/linkedlist.c
+++ src/data-structures/linkedlist/linkedlist.c
@@ -2,15 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include <linkedlist.h>
-#include <utils.h>
+#include "data-structures/linkedlist/linkedlist.h"
+#include "utils/utils.h"
 
 
 // Allocates a new Node in memory with the given values and returns its pointer
 Node*
 new_node(NodeItem item, Node* next, Node* prev)
 {
-    Node* n = xmalloc(sizeof *n);
+    Node* n = malloc(sizeof *n);
+
+    if (n == NULL)
+        return NULL;
+
     n->item = item;
     n->next = next;
     n->prev = prev;
@@ -21,7 +25,11 @@ new_node(NodeItem item, Node* next, Node* prev)
 LinkedList*
 new_linkedlist(void)
 {
-    LinkedList* ll = xmalloc(sizeof *ll);
+    LinkedList* ll = malloc(sizeof *ll);
+
+    if (ll == NULL)
+        die(__func__, ERR_ALLOC_FAILED);
+
     ll->head = NULL;
     ll->tail = NULL;
     ll->size = 0;
@@ -32,6 +40,9 @@ new_linkedlist(void)
 bool
 is_empty(LinkedList* ll)
 {
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
     return ll->head == NULL && ll->tail == NULL && ll->size == 0;
 }
 
@@ -39,14 +50,29 @@ is_empty(LinkedList* ll)
 void
 push_back(LinkedList* ll, NodeItem item)
 {
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
     if (is_empty(ll)) {
         ll->head = new_node(item, NULL, NULL);
+
+        if (ll->head == NULL) {
+            free_linkedlist(ll);
+            die(__func__, ERR_ALLOC_FAILED);
+        }
+
         ll->tail = ll->head;
         ll->size++;
         return;
     }
 
     ll->tail->next = new_node(item, NULL, ll->tail);
+
+    if (ll->tail->next == NULL) {
+        free_linkedlist(ll);
+        die(__func__, ERR_ALLOC_FAILED);
+    }
+
     ll->tail = ll->tail->next;
     ll->size++;
     return;
@@ -56,12 +82,21 @@ push_back(LinkedList* ll, NodeItem item)
 void
 push_front(LinkedList* ll, NodeItem item)
 {
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
     if (is_empty(ll)) {
         push_back(ll, item);
         return;
     }
 
     ll->head = new_node(item, ll->head, NULL);
+
+    if (ll->head == NULL) {
+        free_linkedlist(ll);
+        die(__func__, ERR_ALLOC_FAILED);
+    }
+
     ll->head->next->prev = ll->head;
     ll->size++;
     return;
@@ -71,6 +106,9 @@ push_front(LinkedList* ll, NodeItem item)
 NodeItem
 pop_back(LinkedList* ll)
 {
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
     if (!is_empty(ll)) {
         if (ll->size == 1) {
             NodeItem item = ll->head->item;
@@ -96,13 +134,16 @@ pop_back(LinkedList* ll)
     }
 
     free_linkedlist(ll);
-    die("list not long enough to pop back");
+    die(__func__, ERR_OUT_OF_BOUNDS);
 }
 
 // Removes and returns the first item of the list
 NodeItem
 pop_front(LinkedList* ll)
 {
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
     if (!is_empty(ll)) {
         if (ll->size == 1) {
             NodeItem item = pop_back(ll);
@@ -122,47 +163,180 @@ pop_front(LinkedList* ll)
     }
 
     free_linkedlist(ll);
-    die("list not long enough to pop front");
+    die(__func__, ERR_OUT_OF_BOUNDS);
 }
 
-// // TODO
-// // Reverses the list
-// void
-// reverse(LinkedList* ll)
-// {}
+// Reverses the list
+void
+reverse_list(LinkedList* ll)
+{
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
 
-// // TODO
-// // Returns the nth item from the end of the list
-// NodeItem
-// nth_item_from_end(LinkedList* ll, int n)
-// {}
+    if (ll->size > 1) {
+        Node* next;
 
-// // TODO
-// // Inserts given item at index
-// void
-// insert(LinkedList* ll, int index, NodeItem item)
-// {}
+        for (Node* node = ll->head; node != NULL; node = next) {
+            next = node->next;
+            node->next = node->prev;
+            node->prev = next;
+        }
 
-// // TODO
-// // Removes the first item in the list equal to the given item
-// void
-// remove_item(LinkedList* ll, NodeItem item)
-// {}
+        Node* head = ll->head;
+        ll->head = ll->tail;
+        ll->tail = head;
+    }
 
-// // TODO
-// // Removes and returns the item at the given index
-// NodeItem
-// remove_index(LinkedList* ll, int index)
-// {}
+    return;
+}
+
+// Inserts given item at index
+void
+insert_item(LinkedList* ll, int index, NodeItem item)
+{
+    if (index == 0) {
+        push_front(ll, item);
+        return;
+    }
+
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
+    if (!is_within_range(ll, index)) {
+        free_linkedlist(ll);
+        die(__func__, ERR_OUT_OF_BOUNDS);
+    }
+
+    Node* node = get_node_at(ll, index);
+    Node* new = new_node(item, node, node->prev);
+
+    if (new == NULL) {
+        free_linkedlist(ll);
+        die(__func__, ERR_ALLOC_FAILED);
+    }
+
+    node->prev->next = new;
+    node->prev = node->prev->next;
+    ll->size++;
+    return;
+}
+
+// Removes and returns the item at the given index
+NodeItem
+remove_index(LinkedList* ll, int index)
+{
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+
+    if (!is_within_range(ll, index)) {
+        free_linkedlist(ll);
+        die(__func__, ERR_OUT_OF_BOUNDS);
+    }
+
+    if (index == 0)
+        return pop_front(ll);
+
+    if (index == ll->size-1)
+        return pop_back(ll);
+
+    Node* node = get_node_at(ll, index);
+    NodeItem item = node->item;
+
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    ll->size--;
+
+    free(node);
+    return item;
+}
+
+// TODO
+// Removes the first item in the list equal to the given item
+void
+remove_item(LinkedList* ll, NodeItem item)
+{
+    if (ll == NULL)
+        die(__func__, ERR_NULL_VALUE);
+    
+    if (ll->size > 0) {
+        int i = 0;
+        Node* node = ll->head;
+
+        while (node != NULL) {
+            if (node->item == item) {
+                if (i == 0) {
+                    pop_front(ll);
+                    return;
+                }
+
+                if (i == ll->size-1) {
+                    pop_back(ll);
+                    return;
+                }
+
+                node->prev->next = node->next;
+                node->next->prev = node->prev;
+                ll->size--;
+
+                free(node);
+                return;
+            }
+
+            i++;
+            node = node->next;
+        }
+    }
+
+    return;
+}
+
+
+// INTERNAL FUNCTION
+// Returns the node at the given index
+Node*
+get_node_at(LinkedList* ll, int index)
+{
+    if (!is_within_range(ll, index)) {
+        free_linkedlist(ll);
+        die(__func__, ERR_OUT_OF_BOUNDS);
+    }
+
+    if (index == 0)
+        return ll->head;
+    
+    if (index == ll->size-1)
+        return ll->tail;
+
+    Node* node = ll->head->next;
+
+    for (int i = 1; i < index; i++)
+        node = node->next;
+
+    return node;
+}
+
+// INTERNAL FUNCTION
+// Returns true if given index is within bounds
+bool
+is_within_range(LinkedList* ll, int index)
+{
+    return (index >= 0 && index < ll->size);
+}
 
 // INTERNAL FUNCTION
 // Deallocates linkedlist from memory
 void
 free_linkedlist(LinkedList* ll)
 {
-    for (Node* n = ll->head; n != NULL; n = n->next)
-        free(n);
+    if (ll != NULL) {
+        for (Node* n = ll->head; n != NULL;) {
+            Node* tmp = n->next;
+            free(n);
+            n = tmp;
+        }
 
-    free(ll);
+        free(ll);
+    }
+
     return;
 }
--- src/data-structures/linkedlist/linkedlist.h
+++ src/data-structures/linkedlist/linkedlist.h
@@ -1,6 +1,8 @@
 #ifndef XDOC_LINKEDLIST_H
 #define XDOC_LINKEDLIST_H
 
+#include <stdbool.h>
+
 typedef int NodeItem;
 typedef struct LinkedList LinkedList;
 typedef struct Node Node;
@@ -19,15 +21,22 @@ struct LinkedList {
 
 Node* new_node(NodeItem item, Node* next, Node* prev);
 LinkedList* new_linkedlist(void);
+
 bool is_empty(LinkedList* ll);
+
 void push_back(LinkedList* ll, NodeItem item);
 void push_front(LinkedList* ll, NodeItem item);
+void reverse_list(LinkedList* ll);
+void insert_item(LinkedList* ll, int index, NodeItem item);
+void remove_item(LinkedList* ll, NodeItem item);
+
 NodeItem pop_back(LinkedList* ll);
 NodeItem pop_front(LinkedList* ll);
-NodeItem* items(LinkedList* ll);
-NodeItem* items_from_end(LinkedList* ll);
+NodeItem remove_index(LinkedList* ll, int index);
 
 // INTERNAL FUCNTIONS
 void free_linkedlist(LinkedList* ll);
+bool is_within_range(LinkedList* ll, int index);
+Node* get_node_at(LinkedList* ll, int index);
 
 #endif
